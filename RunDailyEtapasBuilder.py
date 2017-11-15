6d2e0d27181f1e2b2c6d51ea33dc225821c21035
leocamusc <leocamusc@gmail.com>
2017-11-15 15:14:26 -0300

    Filtering ddbb

--- RunDailyEtapasBuilder.py
+++ RunDailyEtapasBuilder.py
@@ -1,5 +1,5 @@
 """Module to run the complete methodology for building histograms, and build it"""
-from Utils import SimplifyEtapasUtils
+from Utils import SimplifyFilesUtils
 from Utils import HeadersUtils
 from Utils import TransantiagoConstants
 import pandas as pd
@@ -7,15 +7,22 @@ import os
 
 analyzedDate = input('Enter the date to analyze: ')
 analyzedVehicle = input('Enter the vehicle to analyze: ')
+if_ZP = input('Consider ZP in perfiles file (0/1)?: ')
+
 SSHDir = TransantiagoConstants.SSHDir
 busesTorniqueteDir = TransantiagoConstants.busesTorniqueteDir
 currentSSHDates = TransantiagoConstants.currentSSHDates
 
 def runSimplifyEtapas():
-	"""Not always necessary. To simplify"""
-	SimplifyEtapasUtils.writeSimplifiedEtapas(analyzedDate,analyzedVehicle,'id','nviaje','tipo_transporte','t_subida','servicio_subida','par_subida','sitio_subida')
+	"""Not always necessary. Reducing the complexity of the original etapas-file"""
+	SimplifyFilesUtils.writeSimplifiedEtapas(analyzedDate,analyzedVehicle,'id','nviaje','tipo_transporte','t_subida','servicio_subida','par_subida','sitio_subida')
+
+def runSimplifyPerfiles():
+	"""Not always necessary. Reducing the complexity of the original etapas-file"""
+	SimplifyFilesUtils.writeSimplifiedPerfiles(analyzedDate,if_ZP,'ServicioSentido','Patente','Paradero','Hini','Hfin','idExpedicion','DistEnRuta')
 
 def loadSimplifiedEtapas():
+	"""Returns the simplified etapas-file as pandas df"""
 	try:
 		if analyzedDate in currentSSHDates:
 			simplifiedEtapasFile = analyzedDate + '_simplified.etapas'
@@ -25,20 +32,39 @@ def loadSimplifiedEtapas():
 	except ValueError as dateErr:
 		print(dateErr)
 
-	etapas_df = pd.read_table(simplifiedEtapasPath, sep='|')
+	etapas_df = pd.read_table(simplifiedEtapasPath, sep='|', encoding='latin-1')
 	return etapas_df
 
+def mergeIdExpedicion(df):
+	"""Merges the loaded etapas-file with the perfiles-file.idExpedicion into a pandas df and returns it"""
+	try:
+		if analyzedDate in currentSSHDates:
+			simplifiedPerfilesFile = analyzedDate + '_simplified.perfiles'
+			simplifiedPerfilesPath = os.path.join(SSHDir, simplifiedPerfilesFile)	
+		else:
+			raise ValueError('date is not correctly specified')
+	except ValueError as dateErr:
+		print(dateErr)
+
+	perfiles_df = pd.read_table(simplifiedPerfilesPath, sep='|', encoding='latin-1')
+	#TODOTODOTODOTODOTODO
+
+
+	return merged_expedicion_df
+
 def mergeTurnstileData(df):
+	"""Merges the loaded etapas-file with the turnstile-file.fecha_instalacion into a pandas df and returns it"""
 	torniquetesFile = 'Avance_Consolidado_v2.xlsx'
 	torniquetesDataPath = os.path.join(busesTorniqueteDir, torniquetesFile)
 	busesTorniquete_df = pd.read_excel(torniquetesDataPath)
 	busesTorniquete_df.columns=['sitio_subida','fecha_instalacion']
-	merged_df = pd.merge(df,busesTorniquete_df, on='sitio_subida', how='left')
-	checking_missing = pd.isnull(merged_df['fecha_instalacion'])
-	print('Not found in turnstile database: ' + str(sum(checking_missing)))#Without turnstiles.
-	return merged_df
+	merged_turnstiles_df = pd.merge(df,busesTorniquete_df, on='sitio_subida', how='left')
+	checking_missing = pd.isnull(merged_turnstiles_df['fecha_instalacion'])
+	print('Not found in turnstile database: ' + str(sum(checking_missing))) #Without turnstiles, then NaT.
+	return merged_turnstiles_df
 
 def cleanDataFrame(df):
+	"""Returns a clean pandas dataframe without '-' values"""	
 	df = df[df.t_subida != '-']
 	df = df[df.servicio_subida != '-']
 	df = df[df.par_subida != '-']
@@ -46,6 +72,7 @@ def cleanDataFrame(df):
 	return df
 
 def sortDataFrame(df):
+	"""Sorts the final dataframe by sitio_subida and then by t_subida"""		
 	df['t_subida']=pd.to_datetime(df.t_subida)
-	sortedDataFrame = df.sort_values(['sitio_subida', 't_subida'], ascending=[True, True])
+	sortedDataFrame = df.sort_values(by=['sitio_subida', 't_subida'], ascending=[True, True])
 	return sortedDataFrame
---
+++ Utils/SimplifyFilesUtils.py
@@ -0,0 +1,87 @@
+"""Module to reduce the size of the 'Transantiago' files"""
+
+#import HeadersUtils
+#import TransantiagoConstants
+import os
+
+#For Jupyter.
+from Utils import HeadersUtils
+from Utils import TransantiagoConstants
+
+SSHDir = HeadersUtils.SSHDir
+currentSSHDates = TransantiagoConstants.currentSSHDates
+
+def writesimplifiedPerfiless(date,vehicleType,*argv):
+	"""args[0]=date, args[1]=vehicleType, args[2,3,...]=headers to extract"""
+	try:
+		if date in currentSSHDates:
+			workingPerfilesFile = date + '.etapas'
+			simplifiedPerfilesFile = date + '_simplified.etapas'
+			workingPerfilesPath = os.path.join(SSHDir, workingPerfilesFile)
+			simplifiedPerfilesPath = os.path.join(SSHDir,simplifiedPerfilesFile)
+		else:
+			raise ValueError('date is not correctly specified')
+	except ValueError as dateErr:
+		print(dateErr)
+
+	with open(workingPerfilesPath, "r") as workingPerfiles:
+		with open(simplifiedPerfilesPath,"w") as simplifiedPerfiles:
+			firstWorkingLine = workingPerfiles.readline().split('|') #Headers
+			simplifiedFirstWorkingLine = simplifyEtapasLine(firstWorkingLine, *argv)
+			simplifiedPerfiles.write("%s\n"%simplifiedFirstWorkingLine)
+			for workingPerfilesLine in workingPerfiles:
+				splittedworkingPerfilesLine = workingPerfilesLine.split('|')
+				if splittedworkingPerfilesLine[HeadersUtils.getIndexOfAttribute('etapas','tipo_transporte')]==vehicleType:
+					simplifiedPerfilesString = simplifyEtapasLine(splittedworkingPerfilesLine, *argv)
+					simplifiedPerfiles.write("%s\n"%simplifiedPerfilesString)
+		simplifiedPerfiles.close()
+	workingPerfiles.close()
+
+def writeSimplifiedPerfiles(date,ZP,*argv):
+	"""args[0]=date, args[1]=ZP, args[2,3,...]=headers to extract"""
+	try:
+		if date in currentSSHDates:
+			workingPerfilesFile = date + '.perfiles'
+			simplifiedPerfilesFile = date + '_simplified.perfiles'
+			workingPerfilesPath = os.path.join(SSHDir, workingPerfilesFile)
+			simplifiedPerfilesPath = os.path.join(SSHDir,simplifiedPerfilesFile)
+		else:
+			raise ValueError('date is not correctly specified')
+	except ValueError as dateErr:
+		print(dateErr)
+
+	with open(workingPerfilesPath, "r") as workingPerfiles:
+		with open(simplifiedPerfilesPath,"w") as simplifiedPerfiles:
+			firstWorkingLine = workingPerfiles.readline().split('|') #Headers
+			simplifiedFirstWorkingLine = simplifyPerfilesLine(firstWorkingLine, *argv)
+			simplifiedPerfiles.write("%s\n"%simplifiedFirstWorkingLine)
+			for workingPerfilesLine in workingPerfiles:
+				splittedworkingPerfilesLine = workingPerfilesLine.split('|')
+				if splittedworkingPerfilesLine[HeadersUtils.getIndexOfAttribute('perfiles','ZP')]==ZP:
+					simplifiedPerfilesString = simplifyPerfilesLine(splittedworkingPerfilesLine, *argv)
+					simplifiedPerfiles.write("%s\n"%simplifiedPerfilesString)
+		simplifiedPerfiles.close()
+	workingPerfiles.close()
+
+
+def simplifyEtapasLine(line, *argv):
+	"""args[0]=currentLine, args[1,2,...]=headers to extract"""
+	simplifiedLine = []
+	for attribute in argv:
+		indexOfAttribute = HeadersUtils.getIndexOfAttribute('etapas',attribute)
+		attributeToInclude = line[indexOfAttribute]
+		simplifiedLine.append(attributeToInclude)
+
+	simplifiedPerfilesString = "|".join(simplifiedLine)
+	return simplifiedPerfilesString
+
+def simplifyPerfilesLine(line, *argv):
+	"""args[0]=currentLine, args[1,2,...]=headers to extract"""
+	simplifiedLine = []
+	for attribute in argv:
+		indexOfAttribute = HeadersUtils.getIndexOfAttribute('perfiles',attribute)
+		attributeToInclude = line[indexOfAttribute]
+		simplifiedLine.append(attributeToInclude)
+
+	simplifiedPerfilesString = "|".join(simplifiedLine)
+	return simplifiedPerfilesString
