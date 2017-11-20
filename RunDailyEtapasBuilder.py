d4180e7b4e54ad1f9a456f308db66e71edbaf567
leocamusc <leocamusc@gmail.com>
2017-11-20 17:16:17 -0300

    Committing work of 20.11

--- RunDailyEtapasBuilder.py
+++ RunDailyEtapasBuilder.py
@@ -3,7 +3,9 @@ from Utils import SimplifyFilesUtils
 from Utils import HeadersUtils
 from Utils import TransantiagoConstants
 import pandas as pd
+import numpy as np
 import os
+import datetime as dt
 
 analyzedDate = input('Enter the date to analyze: ')
 analyzedVehicle = input('Enter the vehicle to analyze: ')
@@ -35,9 +37,8 @@ def loadSimplifiedEtapas():
 	etapas_df = pd.read_table(simplifiedEtapasPath, sep='|', encoding='latin-1')
 	return etapas_df
 
-#TODO: End this sometime
-def mergeIdExpedicion(df):
-	"""Merges the loaded etapas-file with the perfiles-file.idExpedicion into a pandas df and returns it"""
+def loadSimplifiedPerfiles():
+	"""Returns the simplified perfiles-file as pandas df"""
 	try:
 		if analyzedDate in currentSSHDates:
 			simplifiedPerfilesFile = analyzedDate + '_simplified.perfiles'
@@ -48,8 +49,25 @@ def mergeIdExpedicion(df):
 		print(dateErr)
 
 	perfiles_df = pd.read_table(simplifiedPerfilesPath, sep='|', encoding='latin-1')
-	perfiles_df = perfiles_df[perfiles_df[]]
-	return merged_expedicion_df
+	return perfiles_df
+
+def mergeIdExpedicion(etapas_df):
+	"""Merges the loaded etapas-file with the perfiles-file.idExpedicion into a pandas df and returns it. Consider printing to a file"""
+	#Begin not-so-ugly coding
+	perfiles_df = loadSimplifiedPerfiles()
+	etapas_df['t_subida'] = pd.to_datetime(etapas_df.t_subida)
+	perfiles_df['Hini'] = pd.to_datetime(perfiles_df.Hini)
+	perfiles_df['Hfin'] = pd.to_datetime(perfiles_df.Hfin)
+	for etapas_index, etapas_row in etapas_df.iterrows():
+		for perfiles_index, perfiles_row in perfiles_df.iterrows():
+			if ((etapas_row['servicio_subida']==perfiles_row['ServicioSentido']) &
+				(etapas_row['sitio_subida']==perfiles_row['Patente']) &
+				(etapas_row['par_subida']==perfiles_row['Paradero']) &
+				(perfiles_row['Hini']<=etapas_row['t_subida']<=perfiles_row['Hfin'])):
+				etapas_row['id_expedicion']=perfiles_row['idExpedicion']
+				break
+	#End not-so-ugly coding
+	return etapas_df
 
 def mergeTurnstileData(df):
 	"""Merges the loaded etapas-file with the turnstile-file.fecha_instalacion into a pandas df and returns it"""
