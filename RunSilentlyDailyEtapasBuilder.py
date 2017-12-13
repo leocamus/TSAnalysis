f26a27836f1440759787e0ba8efa7982c24fc721
leocamusc <leocamusc@gmail.com>
2017-12-13 16:59:22 -0400

    Incomplete work 13.12.2017

--- RunSilentlyDailyEtapasBuilder.py
+++ RunSilentlyDailyEtapasBuilder.py
@@ -37,6 +37,14 @@ class RunSilentlyDailyEtapasBuilderClass:
 		[filtered_df,filtered_turnstile_df,filtered_no_turnstile_df] = etapas_builder.filteringDf(processed_sorted_df)
 		return filtered_df
 
+	def runRawProcess(self):
+		etapas_builder = RunSilentlyDailyEtapasBuilderClass(self.analyzedDate,self.analyzedVehicle,self.if_ZP)
+		etapas_df = etapas_builder.loadSimplifiedEtapas()
+		merged_df = etapas_builder.mergeTurnstileData(etapas_df)
+		clean_df = etapas_builder.cleanDataFrame(merged_df)
+		sorted_df = etapas_builder.sortDataFrame(clean_df)
+		return sorted_df
+
 	def runSimplifyEtapas(self):
 		"""Not always necessary. Reducing the complexity of the original etapas-file"""
 		SimplifyFilesUtils.writeSimplifiedEtapas(self.analyzedDate,self.analyzedVehicle,'id','nviaje','tipo_transporte','t_subida','servicio_subida','par_subida','sitio_subida')
