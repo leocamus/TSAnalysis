45690310061f7db1bc334dcfa09b2d2f51c88c47
Diego <despinozas@ing.uchile.cl>
2018-01-17 18:31:43 -0300

    Outliers analysis

--- Developing/tiempo_expedicion.R
+++ Developing/tiempo_expedicion.R
@@ -11,6 +11,7 @@ getLibraries <- function(libs) {
   }
 }
 
+
 libs <- c("data.table",
           "plyr",
           "lubridate",
@@ -42,12 +43,11 @@ for (i in file_name) {
   dframe$Date <- as.Date(dframe$Hini)
   dframe$ti <- format(as.POSIXct(dframe$Hini) ,format = "%H:%M:%S")
   dframe$tf <- format(as.POSIXct(dframe$Hfin) ,format = "%H:%M:%S")
-
+  
   
   #Adding expedition time
   dframe$texpedicion <- period_to_seconds(hms(dframe$tf) - hms(dframe$ti))/60
-
-  #Expedition time analysis
+  
   #Adding expedition time to negative values
   dframe_neg <- dframe[which(dframe$texpedicion < 0),]
   
@@ -56,14 +56,52 @@ for (i in file_name) {
   dframe_neg$texp_corr <- dframe_neg$ti_0 + dframe_neg$tf_1
   
   dframe$texpedicion[which(dframe$texpedicion < 0)] <- period_to_seconds(dframe_neg$texp_corr)/60
-                                                                                                                          
-  #Boxplot
-  boxplot_texp <- boxplot.stats(dframe$texpedicion, coef = 3)
   
-  #Saving table
+  #Expedition time analysis
+  #We split the data into several dataframe by bus line and time period.
+  expediciones <- split(dframe, list(dframe$ServicioSentido, dframe$PeriodoTSExpedicion))
+  
+  #Deleting dataframe without data
+  expediciones <- Filter(function(x) dim(x)[1] > 0, expediciones)
+  
+  #Boxplot per dataframe
+  exped_s_out <- lapply(expediciones, 
+                        FUN = function(dataframe){
+                          
+                          pdf(file = paste(unique(dataframe$ServicioSentido),"-",unique(dataframe$PeriodoTSExpedicion), ".pdf", sep = ""))
+                          
+                          #Plot frame
+                          par(mfrow=c(3, 2), oma=c(0,0,3,0))
+                          
+                          #Boxplot and points with extreme values
+                          boxplot(dataframe$texpedicion, main="Full data")
+                          plot(1:nrow(dataframe), dataframe$texpedicion, xlab = "Observations", ylab = "Expedition Time (min)")
+                          
+                          #Identifying outliers
+                          boxplot_out <- boxplot.stats(dataframe$texpedicion, coef = 1.5)
+                          
+                          #Identifying extreme values
+                          boxplot_extreme <- boxplot.stats(dataframe$texpedicion, coef = 3)
+                          
+                          #Deleting outliers and extreme values
+                          dtframe_out <- subset(dataframe, !(dataframe$texpedicion %in% boxplot_out$out))
+                          dtframe_extreme <- subset(dataframe, !(dataframe$texpedicion %in% boxplot_extreme$out))
+                          
+                          #Boxplot and observations without extreme values
+                          boxplot(dtframe_extreme$texpedicion, main="Without extreme values")
+                          plot(1:nrow(dtframe_extreme), dtframe_extreme$texpedicion, xlab = "Observations", ylab = "Expedition Time (min)")
+                          
+                          #Boxplot and points without outliers
+                          boxplot(dtframe_out$texpedicion, main="Without outliers")
+                          plot(1:nrow(dtframe_out), dtframe_out$texpedicion, xlab = "Observations", ylab = "Expedition Time (min)")
+                          title(paste(unique(dataframe$ServicioSentido),unique(dataframe$PeriodoTSExpedicion), sep = "-"), outer=TRUE)
+                          
+                          dev.off()
+                          
+                        })
+  
   
 
-  
 }
 
 
